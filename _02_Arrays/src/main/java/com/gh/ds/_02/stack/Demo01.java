fba7516a1733444a1ae5edee3483753a8db44dce
gaohui <gaohui@hupu.com>
2020-04-26 23:06:00 +0800

    栈 及其应用

--- _02_Arrays/src/main/java/com/gh/ds/_02/stack/Demo01.java
+++ _02_Arrays/src/main/java/com/gh/ds/_02/stack/Demo01.java
@@ -1,26 +1,37 @@
 package com.gh.ds._02.stack;
 
 import java.util.ArrayDeque;
+import java.util.Stack;
 
 public class Demo01 {
 
     /**
+     * java.util.Stack
+     *
+     * <p>
      * ArrayDeque 可以作为一种 Stack  的数据结构来使用
      * Stack   :  push   pop   peek
+     * <p>
+     * 是一种操作受限的线性结构 , 可以基于数组或者链表来进行实现
      */
     public static void main(String[] args) {
 
-        ArrayDeque<String> deque = new ArrayDeque<String>();
-        deque.push("a");
-        deque.push("b");
-        deque.push("c");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        stack.push(0);
+        stack.push(5);
+        stack.push(4);
+        stack.push(1);
 
+        System.out.println(stack.size());
 
-        // 看一眼 栈顶元素
-        System.out.println(deque.peek());
+        System.out.println(stack.peek());
 
-        System.out.println(deque.pop());
-        System.out.println(deque.pop());
-        System.out.println(deque.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        System.out.println(stack.size());
+
     }
 }
